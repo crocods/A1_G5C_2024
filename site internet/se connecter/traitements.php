3451faa3189dbe910cee7946469f6895b6c39f3d
jean thomas <46343997+crocods@users.noreply.github.com>
2024-12-13 13:46:16 +0100

    Update traitements.php

--- site internet/se connecter/traitements.php
+++ site internet/se connecter/traitements.php
@@ -1,1 +1,23 @@
 
+<?php
+$mail = $_POST['mail'];
+$MdpClient = $_POST['Mot_de_passe'];
+
+if (empty($mail) || empty($MdpClient)) {
+        die("Tous les champs sont obligatoires.");
+    }
+
+ 	$conn = new mysqli('localhost', 'root', '', 'enchere_art');
+    if (!$conn) {
+        die("Connexion échouée : " . mysqli_connect_error());
+    } else {
+		echo "connexion réussie";
+	}
+
+    $result = $conn->query ("SELECT * FROM client WHERE mailClient = '$mail' AND MdpClient='$MdpClient'");
+    if (mysqli_num_rows($result)>0){
+        echo ;
+    } else {
+        echo "votre e-mail ou votre mots de Passe est incorrect";
+    }
+?>
